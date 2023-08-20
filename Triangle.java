abcdf78a820c086066bd46e7ff7c5d120063eb1c
simplegitrit <127583449+simplegitrit@users.noreply.github.com>
2023-08-20 13:16:55 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,37 @@
+public class Triangle {
+
+    static void pattern(int r , int c){
+        if(r==0) return;
+
+        if(c < r){
+            System.out.print("*");
+            pattern(r, c+1);
+        }
+        else{
+            System.out.println();
+            pattern(r-1, 0);
+        }
+    }
+
+    static void pattern2(int r , int c){
+        if(r==0) return;
+
+        if(c < r){
+            pattern2(r, c+1);
+            System.out.print("*");
+        }
+        else{
+            pattern2(r-1, 0);
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        pattern(4, 0);
+        pattern2(4, 0);
+        
+    }
+    
+}
